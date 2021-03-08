d2754a0269adaf52d6d7c588ee137f7b7b9c891f
Hakiki-XC <80109091+Hakiki-XC@users.noreply.github.com>
2021-03-08 13:24:01 +0700

    install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+rm -rf Fbiv1
+pkg update && pkg upgrade
+pkg install python
+pkg install python2
+pkg install git
+pip install --upgrade pip
+pip install requests
+pip install mechanize
+pip install cookiejar
+pip install bs4
+pip install curl
+pip2 install requests
+pip2 install bs4
+git clone https://github.com/Hakiki-XC/Fbiv1
+cd Fbiv1
+ls
+python2 Crackv1.py
